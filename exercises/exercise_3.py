b3753b4e10fb1bd3fb8465a7a8213dee2a3048c0
raminhasan <ramin.hasan@outlook.com>
2024-06-03 08:50:38 +0000

    sol3 fixed again

--- exercises/exercise_3.py
+++ exercises/exercise_3.py
@@ -4,35 +4,35 @@ userInput = int(input('Input a number from 0 to 36: '))
 
 output = ''
 if userInput == 0:
-  output = 'green'
+  output = 'Green'
 
 elif 1 <= userInput <= 10:
   if userInput % 2 == 1:
-    output ='red'
+    output ='Red'
 
   else:
-    output = 'black'
+    output = 'Black'
 
 elif 11 <= userInput <= 18:
   if userInput % 2 == 1:
-    output ='black'
+    output ='Black'
 
   else:
-    output = 'red'
+    output = 'Red'
 
 elif 19 <= userInput <= 28:
   if userInput % 2 == 1:
-    output ='red'
+    output ='Red'
   
   else:
-    output = 'black'
+    output = 'Black'
 
 elif 29 <= userInput <= 36:
   if userInput % 2 == 1:
-    output ='black'
+    output ='Black'
   
   else:
-    output = 'red'
+    output = 'Red'
 
 else:
   print('The bet will not play!')

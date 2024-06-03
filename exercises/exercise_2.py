ff6c2983661bb140a9852cfc8cd5e7bce14667a9
raminhasan <ramin.hasan@outlook.com>
2024-06-03 08:39:29 +0000

    sol2

--- exercises/exercise_2.py
+++ exercises/exercise_2.py
@@ -1,2 +1,18 @@
 # Your solution to Exercise 2
 
+userAge = int(input('How old are you? '))
+
+output = ''
+if userAge <= 1:
+  output = 'You are an infant.'
+
+elif 1 < userAge < 13:
+  output = 'You are a child.'
+
+elif 13 <= userAge < 20:
+  output = 'You are a teenager.'
+
+else: 
+  output = 'You are an adult.'
+
+print(output)

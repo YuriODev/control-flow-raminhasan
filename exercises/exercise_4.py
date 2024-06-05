67e6af646cf6cf3f7b881bbcb5a96d406226bea7
raminhasan <ramin.hasan@outlook.com>
2024-06-05 17:59:48 +0000

    sol4

--- exercises/exercise_4.py
+++ exercises/exercise_4.py
@@ -1,2 +1,21 @@
 # Your solution to Exercise 4
 
+userInput = int(input("Enter your grade: "))
+
+output = ''
+if 1 <= userInput <= 3:
+  output = 'initital level'
+
+elif 4 <= userInput <= 6:
+  output = 'average level'
+
+elif 7 <= userInput <= 9:
+  output = 'sufficient level'
+
+elif 10 <= userInput <= 12:
+  output = 'high level'
+
+else:
+  output = 'level is absent'
+
+print(output)

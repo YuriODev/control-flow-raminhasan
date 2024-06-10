bc2a852b1a11d4b4c8914dfd6d61c8d17565394d
raminhasan <ramin.hasan@outlook.com>
2024-06-10 11:03:17 +0000

    sol6

--- exercises/exercise_6.py
+++ exercises/exercise_6.py
@@ -1,2 +1,18 @@
 # Your solution to Exercise 6
 
+A1 = int(input())
+A2 = int(input())
+B1 = int(input())
+B2 = int(input())
+
+distA = ((A2 ** 2) + (A1 ** 2)) ** (1/2)  
+distB = ((B2 ** 2) + (B1 ** 2)) ** (1/2)
+
+if distA == distB:
+  print("A and B are at the same distance from the origin.")
+
+elif distA < distB:
+  print("B is further from the origin.")
+
+elif distA > distB:
+  print("A is further from the origin.")

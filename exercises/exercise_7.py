9784178c0842df3f6c96a7739becfa689845c99d
raminhasan <ramin.hasan@outlook.com>
2024-06-12 12:42:03 +0000

    sol7

--- exercises/exercise_7.py
+++ exercises/exercise_7.py
@@ -1,2 +1,33 @@
 # Your solution to Exercise 7
 
+firstNum = float(input())
+secondNum = float(input())
+operation = input()
+
+if operation == "+":
+  print(firstNum + secondNum)
+
+elif operation == "-":
+  print(firstNum - secondNum)
+
+elif operation == "/":
+  if secondNum == 0.0:
+    print("Division by 0!")
+
+  else:
+    print(firstNum / secondNum)
+
+elif operation == "*":
+  print(firstNum * secondNum)
+
+elif operation == "mod":
+  print(firstNum % secondNum)
+
+elif operation == "pow":
+  print(firstNum ** secondNum)
+
+elif operation == "div":
+  print(firstNum // secondNum)
+
+else:
+  print("Error")

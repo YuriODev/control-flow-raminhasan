6b3dd99ef92e9446ac37bd84932bafcab0d88429
raminhasan <ramin.hasan@outlook.com>
2024-06-12 12:50:40 +0000

    sol8

--- exercises/exercise_8.py
+++ exercises/exercise_8.py
@@ -1,2 +1,12 @@
 # Your solution to Exercise 8
 
+userInput = input()
+
+searchList = userInput[0:2]
+searchItem = userInput[-1]
+
+if searchItem in searchList:
+  print("True")
+
+else:
+  print("False")

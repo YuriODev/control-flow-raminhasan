c90a63c6ba905b9f875afc50ca560bea5e938650
raminhasan <ramin.hasan@outlook.com>
2024-06-13 09:32:46 +0000

    sol6

--- exercises/exercise_6.py
+++ exercises/exercise_6.py
@@ -1,18 +1,45 @@
 # Your solution to Exercise 6
 
-A1 = int(input())
-A2 = int(input())
-B1 = int(input())
-B2 = int(input())
+a = float(input("Enter coefficient a: "))
+b = float(input("Enter coefficient b: "))
+c = float(input("Enter coefficient c: "))
 
-distA = ((A2 ** 2) + (A1 ** 2)) ** (1/2)  
-distB = ((B2 ** 2) + (B1 ** 2)) ** (1/2)
+output = "No roots."
 
-if distA == distB:
-  print("A and B are at the same distance from the origin.")
+if a == 0 and b == 0 and c == 0:
+    output = "Infinite roots."
 
-elif distA < distB:
-  print("B is further from the origin.")
+elif a == 0:
+    if b != 0:
+        output = f"{-c / b}"
 
-elif distA > distB:
-  print("A is further from the origin.")
+elif b == 0:
+    if c != 0:
+        #  -(-c / a) ** 0.5
+        x1 = (-c / a) ** 0.5
+        x2 = -(-c / a) ** 0.5
+        output = f"{x1} and {x2}"
+    else:
+        # If coefficient c is 0, set the output to "0"
+        output = "0"
+
+# If neither coefficient a nor b is 0, calculate the discriminant d
+else:
+    d = b ** 2 - 4 * a * c
+
+    # Check if the discriminant d is greater than 0
+    if d > 0:
+        # If d is greater than 0, calculate the roots using
+        # the quadratic formula
+        x1 = (-b + d ** 0.5) / (2 * a)
+        x2 = (-b - d ** 0.5) / (2 * a)
+        output = f"{x1} and {x2}"
+
+    # Check if the discriminant d is equal to 0
+    elif d == 0:
+        # If d is equal to 0, calculate the root using the formula -b / (2 * a)
+        x = -b / (2 * a)
+        output = f"{x}"
+
+# Print the output
+print(output)

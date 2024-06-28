b4cca6adfd2cc00ef59fe07be50fbc54a533f2ab
raminhasan <ramin.hasan@outlook.com>
2024-06-28 10:02:25 +0000

    sol15

--- exercises/exercise_15.py
+++ exercises/exercise_15.py
@@ -4,68 +4,61 @@ day = int(input())
 month = int(input())
 year = int(input())
 
-if year % 4 == 0 and year % 100 != 0 or year % 400 == 0:
-  if month == 2:
-    if day >= 29:
-      day = 1
-      month += 1
+output = ''
 
-    else:
-      day += 1
+leap_year = (year % 4 == 0) and (year % 100 != 0) or year % 400 == 0
 
-  elif month == 1 or 3 or 5 or 7 or 8 or 10:
-    if day >= 31:
-      day = 1
-      month += 1
+if month == 2:
+  if leap_year and day == 29:
+    day = 1
+    month += 1
 
-    else:
-      day += 1
-  elif month == 4 or 6 or 9 or 11:
-    if day >= 30:
-      day = 1
-      month += 1
+  elif not leap_year and day == 28:
+    day = 1
+    month += 1
 
-    else:
-      day += 1
+  elif day > 28 or (leap_year and day > 29):
+    day += 1
 
-  elif month == 12:
-    if day >= 31:
-      day = 1
-      month = 1
-      year += 1
+  else:
+    output = 'Invalid date'
 
-    elif:
-      day += 1
+elif month == 1 or month == 3 or month == 5 or month == 7 or month == 8 or month == 10:
+  if day == 31:
+    day = 1
+    month += 1
+
+  elif day < 31:
+    day += 1
+
+  else: 
+    output = 'Invalid date'
+
+elif month == 4 or month ==  6 or month == 9 or month == 11:
+  if day == 30:
+    day = 1
+    month += 1
+
+  elif day < 30:
+    day += 1
+
+  else:
+    output = 'Invalid date'
+
+elif month == 12:
+  if day == 31:
+    day = 1
+    month = 1
+    year += 1
+
+  elif day < 31:
+    day += 1
+
+  else:
+    output = 'Invalid date'
 
 else:
-  if month == 2:
-    if day >= 28:
-      day = 1
-      month += 1
+  output = 'Invalid date'
 
-    else:
-      day += 1
-
-  elif month == 1 or 3 or 5 or 7 or 8 or 10:
-    if day >= 31:
-      day = 1
-      month += 1
-
-    else:
-      day += 1
-  elif month == 4 or 6 or 9 or 11:
-    if day >= 30:
-      day = 1
-      month += 1
-
-    else:
-      day += 1
-
-  elif month == 12:
-    if day >= 31:
-      day = 1
-      month = 1
-      year += 1
-
-    elif:
-      day += 1
+if output = '':
+  print(f'{day}.{month}.{year}')
